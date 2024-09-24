c995747091b36087d0c2fbf35a1f77ec69ba513f
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2024-09-24 09:27:10 +0530

    Time: 36 ms (91.56%), Space: 16.9 MB (31.73%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -19,10 +19,12 @@ class Solution:
         right = len(nums)-1
 
         mid_index = find_min(nums)
-        print("mid ",mid_index, nums[mid_index] == target, nums[mid_index] ,target)
+        
         if nums[mid_index] == target:
-            print("found mid")
+            
             return mid_index
+
+        
         lo,hi = 0,0
         if nums[mid_index]< target and target<=nums[right]:
             lo = mid_index+1
@@ -32,7 +34,7 @@ class Solution:
             hi = mid_index-1
 
 
-        print(lo, hi)
+        
 
 
         while(lo<=hi):
@@ -52,4 +54,27 @@ class Solution:
 
         
 
-        
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        left, right = 0, n - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            # Case 1: find target
+            if nums[mid] == target:
+                return mid
+
+            # Case 2: subarray on mid's left is sorted
+            elif nums[mid] >= nums[left]:
+                if target >= nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            # Case 3: subarray on mid's right is sorted.
+            else:
+                if target <= nums[right] and target > nums[mid]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
