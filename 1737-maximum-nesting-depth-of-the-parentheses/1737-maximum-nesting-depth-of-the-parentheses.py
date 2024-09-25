88a7eee57923bbdc0b9de93fb397f48072d0bd7a
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2024-09-26 03:25:03 +0530

    Time: 36 ms (48.37%), Space: 16.6 MB (16.45%) - LeetHub

---
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        
+        stack  = []
+        i = 0
+        max_depth = 0
+        while(i<len(s)):
+            print(i)
+            char = s[i]
+            if char == "(":
+                if not stack:
+                    #stack.append("(")
+                    stack.append(1)
+                else:
+                    last_count =  stack[-1]
+                    #stack.append("(")
+                    stack.append(last_count+1)
+
+            elif char == ")":
+                max_depth = max(max_depth,stack[-1])
+                #stack.pop()
+                stack.pop()
+
+            else:
+                pass
+
+            i=i+1
+
+        return max_depth
+
+
+
+
+
+        
