f6ef2c488c5aadfa6c0906ef0c875c4dffd7bec4
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2024-09-26 03:27:25 +0530

    Time: 43 ms (6.99%), Space: 16.7 MB (16.45%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.py
@@ -28,6 +28,36 @@ class Solution:
 
         return max_depth
 
+# in abouve stack only represents previous max_depth, we can just used a variable 
+    def maxDepth(self, s: str) -> int:
+        
+        stack  = 0
+        i = 0
+        max_depth = 0
+        while(i<len(s)):
+            print(i)
+            char = s[i]
+            if char == "(":
+                if not stack:
+                    #stack.append("(")
+                    stack+=1
+                else:
+                    last_count =  stack
+                    #stack.append("(")
+                    stack+=1
+
+            elif char == ")":
+                max_depth = max(max_depth,stack)
+                #stack.pop()
+                stack-=1
+
+            else:
+                pass
+
+            i=i+1
+
+        return max_depth
+
 
 
 
