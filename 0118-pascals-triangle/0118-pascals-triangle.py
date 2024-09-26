e0d824b4c6c8012d675b538962734e14020c5e22
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2024-09-27 02:56:57 +0530

    Time: 39 ms (39.28%), Space: 16.5 MB (34.05%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+
+        def sol(k):
+            ans =[[1]]
+            if k>=2:
+                ans =[[1],[1,1]]
+            while(k>2):
+
+                row = [1]
+                prev_row = ans[-1]
+                for index in range(1,len(ans[-1])):
+                    row.append(prev_row[index-1]+ prev_row[index])
+                row.append(1)
+
+                ans.append(row)
+                k-=1
+
+            return ans
+
+        return sol(numRows)
+                
+
+        
