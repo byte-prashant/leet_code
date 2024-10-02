73b6039ede5f7dd152f6ef093c28b9b07f1d052b
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2024-10-03 03:30:18 +0530

    Time: 79 ms (11%), Space: 17.1 MB (5.41%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -29,7 +29,7 @@ class Solution:
         return  [keys for keys in res.keys()]
 
         def sol():
-            candidates.sort()
+            #candidates.sort()
             ret = []
 
             def visit(idx, target, arr):
