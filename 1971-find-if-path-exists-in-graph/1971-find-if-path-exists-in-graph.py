0a28864d81e06ee3f1f636f48541e994895175f3
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-04-19 12:16:43 +0530

    Time: 251 ms (85.02%), Space: 84.8 MB (90.15%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        parent = [ i for i in range(n)]
+        size = [0]*n
+
+        def find(node):
+            if parent[node]!=node:
+                parent[node] = find(parent[node])
+
+            return parent[node]
+
+        def union(node1,node2):
+            parent1 = find(node1)
+            parent2 = find(node2)    
+
+            if parent1!=parent2:
+                parent[parent1] = parent2
+
+        
+        for edge in edges:
+            union(edge[0],edge[1])
+        print(parent)
+        return find(source)==find(destination)
+
+           
