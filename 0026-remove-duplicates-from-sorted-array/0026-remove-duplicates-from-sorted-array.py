33ca719f82d5d3bb506bb574af5ef9a7a74d7ddd
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-04-25 14:32:30 +0530

    Time: 3 ms (44.43%), Space: 20.5 MB (45.68%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -27,6 +27,24 @@ class Solution:
                 nums[left]= nums[right]
 
         return left+1
+
+
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        if len(nums) ==1:
+            return 1
+
+        # will not work
+        # recorrect it
+        # there is learning why cant we use right-left to detect if we need to move and need to do replace
+        left =0
+        for right in range(len(nums)):
+            if nums[right]>nums[left]:
+                left+=1
+                if right-left>0:
+                    nums[left]= nums[right]
+
+        return left+1 
                    
               
 
