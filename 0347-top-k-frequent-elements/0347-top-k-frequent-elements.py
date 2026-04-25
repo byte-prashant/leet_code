e09099509a7c54c942bae41d6ad8cc2f161c9112
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-04-25 14:14:59 +0530

    Time: 3 ms (88.52%), Space: 22.9 MB (43.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -6,7 +6,31 @@ class Solution:
         frequencies = [(elem, freq) for elem ,freq in frequencies.items()]
 
         # using quick select
+        # this is not quick select
+        # but as order does not matter we do not need to sort it fully
+        # we can use quick select
+        # and pick always right side, untill lenght is k
         frequencies.sort(key = lambda x: x[1])
         #print(frequencies)
         return [ ele[0] for  ele in frequencies[len(frequencies)-k:]]
+
+    # trying quick select
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        from collections import Counter
+        frequencies = Counter(nums)
+        frequencies = [(elem, freq) for elem ,freq in frequencies.items()]
+
+        # using quick select
+        # but as order does not matter we do not need to sort it fully
+        # we can use quick select
+        # and pick always right side, untill lenght is k
+        last_elem = None
+        while len(frequencies)>k:
+            pivot ,last_elem= frequencies[-1][1],frequencies[-1]
+            frequencies = [ ele for ele in frequencies if ele[1]>pivot ]
+            
+
+
+        return [ ele[0] for  ele in frequencies] if frequencies else [last_elem[0]]
         
