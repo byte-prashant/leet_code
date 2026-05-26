e645bb780c49e4d8fb5732141dee3ac0ee5432f5
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-05-26 23:48:50 +0530

    Time: 631 ms (5.13%), Space: 20.6 MB (67.62%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        open_brack = []
+        remove_indexes = []
+        count =0
+        new_str = s
+        pos = 0
+        for index, ch in enumerate(s):
+            if ch == ")":
+                if open_brack:
+                   open_brack.pop(-1)
+                else:
+                   remove_indexes.append(index)
+            elif ch == "(":
+                open_brack.append(index)
+        if open_brack:
+            remove_indexes.extend(open_brack)
+        new_s = s
+        
+        if remove_indexes:
+            new_s =""
+            remove_indexes.sort()
+            print(remove_indexes)
+            for index , ch in  enumerate(s):
+                if not index in remove_indexes:
+                    new_s+=ch
+
+        return new_s
+                
+
+        
+
+        
+
+        
+
+        
+                
+
+        
