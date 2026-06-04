4f1f54db014bc9a5195ca06a45267da926bbf9c9
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-06-04 22:18:03 +0530

    Time: 115 ms (80.31%), Space: 19.3 MB (37.72%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -14,5 +14,24 @@ class Solution:
             ans+=expand(i,i+1,s)
         return ans
 
+    def countSubstrings(self, s: str) -> int:
+
+        def expand(i,j,s):
+            count = 0
+            while i>=0 and j<len(s) and s[i] == s[j]:
+                count+=1
+                i-=1
+                j+=1
+            return count
+        ans=0
+        for i in range(len(s)):
+            ans+=expand(i,i,s)
+            ans+=expand(i,i+1,s)
+        return ans
+
+            
+
+
+
       
 
