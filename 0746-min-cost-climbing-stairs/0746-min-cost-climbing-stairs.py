4b7f7c65fb78a4c618b43c745f9e6536d68baae2
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-06-27 16:47:17 +0530

    Time: 8 ms (6.81%), Space: 19.5 MB (25.85%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -34,3 +34,26 @@ class Solution:
 
         
         return min(recursive(len(cost)-1),recursive(len(cost)-2))
+
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        #dp = {}
+
+        # in yop down approach all dp states are represneted by one variable
+        # so we can explore all state in single loop
+        # converting to bootom up approach
+        dp = [0]*(len(cost)+1)
+         #init base case
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        n = len(cost)
+        # now exploring all states
+        for state in range(2,len(cost)):
+            print(state)
+            dp[state] = cost[state] + min(dp[state-1],dp[state-2])
+
+        return min(dp[n-1],dp[n-2])
+       
+
+        
+    
