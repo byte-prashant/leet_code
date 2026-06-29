c5ecbac023e82b78178160f17c799ca14965b3b5
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-06-29 16:49:58 +0530

    Time: 1379 ms (5.24%), Space: 24.8 MB (21.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -90,6 +90,7 @@ class Solution:
         # state reprensented by (pos, amount)
         # pos ==> range(0, len(coins)), amount => range(min, max) ==> min =>  we continuous decreasing from amoun,  minimum amout will be if we decreat it by max value of coin by n time
         # min  = amount - (n* max(coins))
+        # but as all amount <0 has been ignored, we can start loop from 1 to amount
         # maxx. = if we do not minimize anything, amount itself
 
 
@@ -123,6 +124,54 @@ class Solution:
         ans = dp.get((0, amount),float("inf"))
         return -1 if ans == float("inf") else ans
 
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        minn = [float("inf")]
+        # converting to bottom up
+        #  we  each state  is reprented by  two vars
+        # count has been removed
+        #  state pos depends on  pos+1
+        # state reprensented by (pos, amount)
+        # pos ==> range(0, len(coins)), amount => range(min, max) ==> min =>  we continuous decreasing from amoun,  minimum amout will be if we decreat it by max value of coin by n time
+        # min  = amount - (n* max(coins))
+        # but as all amount <0 has been ignored, we can start loop from 1 to amount
+        # maxx. = if we do not minimize anything, amount itself
+
+
+        #amount = (amount - (n* max(coins)),amount)
+
+        # recurrent
+        # dp[(pos+1, amount)], dp[(pos+1, amount-coins[pos])]
+
+        # base 
+        #  by default 0, whenever amount ==0, return zero, if less than zeo, it is inf
+
+        # we can use matrix or dict anything
+
+
+        # using mattrix instead of dict
+        # how to choose, row and colum
+        dp = [[float("inf") for _ in range(amount + 1)] for _ in range(len(coins) + 1)]
+        
+        for pos in range(len(coins)-1,-1,-1):
+            dp[pos][0] = 0
+        for amt in range(1, amount + 1): 
+            dp[len(coins)] [amt] = float("inf")
+
+        for pos in range(len(coins)-1,-1,-1):
+            for amount in range(1 ,amount+1):
+                skip = dp[pos+1][ amount]
+                if amount >= coins[pos]: 
+                    take = dp[pos][ amount - coins[pos]] + 1 
+                else: 
+                    take = float("inf")
+                    #coin(pos+1, amount-coins[pos],count+1)
+                dp[pos][amount] = min(skip, take)
+                    
+
+        ans = dp[0][amount]
+        return -1 if ans == float("inf") else ans
             
 
         
