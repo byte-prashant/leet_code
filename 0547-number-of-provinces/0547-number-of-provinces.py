1bb9e2c19479e95df7d33b44d271d85d3baa94c1
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-07-02 23:57:43 +0530

    Time: 15 ms (11.85%), Space: 20.6 MB (47.94%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -23,6 +23,51 @@ class Solution:
 
         return provinces
 
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        parent = [city for city in range(n)]
+        size = [1 for _  in range(n)]
+
+        def find_parent(node):
+            if parent[node]!=node:
+                parent[node] = find_parent(parent[node])
+
+            return parent[node]
+
+        def union(node1,node2):
+
+            p1 = find_parent(node1)
+            p2 = find_parent(node2)
+
+            if p1!=p2:
+                if size[p1]>size[p2]:
+                    parent[p2] = p1
+                    size[p1]+=size[p2]
+                    size[p2] =0
+
+                else:
+                    parent[p1] = p2
+                    size[p2]+=size[p1]
+                    size[p1] =0
+
+        
+        for row in range(len(isConnected)):
+            for col in range(len(isConnected[0])):
+                if isConnected[row][col] ==1:
+                    union(row,col)
+
+
+        count =0 
+
+        for s in size:
+            if s>0:
+                count+=1
+
+        return count
+
+
+
+
 
 
             
