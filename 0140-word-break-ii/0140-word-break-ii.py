b3579dfa25de909b48da6bd9a6ad961f6b3e3352
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-07-03 12:29:49 +0530

    Time: 0 ms (100%), Space: 19.4 MB (53.36%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -25,3 +25,41 @@ class Solution:
         sol(0, len(s), [])
 
         return ans
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        
+        dp = {}
+        ans = []
+        def sol(start, end):
+            if (start, end) in dp:
+                return dp[(start,end)]
+
+            results = []
+            if end<=start:
+                
+                results.append("")
+                return results
+
+            for k in range(start,end):
+
+                if s[start:k+1] in wordDict:
+                    
+                    word = s[start:k+1]
+                    sub_list = sol(k+1,end )
+                    for sub in sub_list:
+
+                        if sub:
+                            results.append(f"{word} {sub}")
+                        else:
+                            results.append(word)
+
+                    
+
+            dp[(start,end)] = results
+
+            return   dp[(start,end)]
+
+      
+
+        return    sol(0, len(s)) 
