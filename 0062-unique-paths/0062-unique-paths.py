a16d41b9233797e810bdff0f17e9a890939b950a
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-07-05 00:45:16 +0530

    Time: 0 ms (100%), Space: 19.6 MB (14.01%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -27,6 +27,48 @@ class Solution:
 
         return sol(0,0)
 
+# cover to bootom up
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+
+        directions = [(0,1),(1,0)]
+
+        dp = [[0] * n for _ in range(m)]
+        def is_valid(x,y):
+            return x<m and x>=0 and y>=0 and y<n
+
+        # base case, final index will be 1
+
+        dp[m-1][n-1] =1
+
+        # loop, range =>x ==>[0,m],y=>[0,n]
+        # order x  dependendant on x+dx, similary y => y+dx
+
+        for x in range(m-1, -1,-1):
+            for y in range(n-1,-1,-1):
+                if x == m - 1 and y == n - 1:
+                    continue
+
+                right = 0
+                down = 0
+
+                if y+1 < n:
+                    right = dp[x][y+1]
+
+                if x+1 < m:
+                    down = dp[x+1][y]
+
+                dp[x][y] = right + down
+
+        return dp[0][0]
+
+
+
+        
+
+       
+      
+
             
 
         
