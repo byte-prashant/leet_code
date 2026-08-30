c1a6b0e613b869555235cf6c85cef7f15280fd67
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-08-30 14:22:41 +0530

    Time: 0 ms (100%), Space: 19.5 MB (11.14%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -39,3 +39,38 @@ class Solution:
 
 
 
+class Solution:
+    def generateMatrix(self, n: int) -> List[List[int]]:
+
+        pos = (0,0)
+        directions = [(0,1),(1,0),(0,-1),(-1,0)]
+        matrix = [[0 for _ in range(n)] for j in range(n)]
+
+        change = 0
+        new_dir = 0
+        num = 1
+        new_row, new_col = pos[0],pos[1]
+        while change<=1:
+
+
+            row,col = new_row,new_col
+            if row<n and row>=0 and col>=0 and col<n and  matrix[row][col]==0:
+                matrix[row][col] = num
+                change = 0
+                num+=1
+
+            else:
+                new_dir =(new_dir+1)%len(directions)
+                new_direction = new_dir
+                change+=1
+                # IMPORTANT:
+                # Go back to the previous valid position
+                row -= directions[(new_dir - 1) % 4][0]
+                col -= directions[(new_dir - 1) % 4][1]
+
+
+            dx,dy = directions[new_dir][0],directions[new_dir][1]
+            new_row, new_col = row+dx,col+dy
+            print(new_row, new_col )
+
+        return matrix
