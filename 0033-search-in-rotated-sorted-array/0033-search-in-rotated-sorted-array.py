889853310b0f6d3f9b55b1ab4f3666479ea34bd6
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-08-30 23:38:45 +0530

    Time: 0 ms (100%), Space: 19.6 MB (10.43%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -55,6 +55,21 @@ class Solution:
         
 
     def search(self, nums: List[int], target: int) -> int:
+
+        # first check if we are in rotated array or not
+
+             # if mid> left:
+
+                # we are not in rotated array
+
+                    # find  if lement is present in this sec
+
+            # elif   mid< left  means we are in rotated arrr
+
+                #  check if element is present in this section
+                # otherwise goto other sections
+
+               
         n = len(nums)
         left, right = 0, n - 1
         while left <= right:
@@ -77,4 +92,32 @@ class Solution:
                     left = mid + 1
                 else:
                     right = mid - 1
+        return -1
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        left = 0
+        right = len(nums)-1
+
+        while  left <= right:
+
+            mid = left + (right-left)//2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] >= nums[left]:
+
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid  - 1
+
+                else:
+                    left = mid+1
+            else:
+
+                if target <= nums[right] and target>=nums[mid]:
+                    left = mid+1
+                else:
+                    right = mid-1
         return -1
