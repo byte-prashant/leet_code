354a45a1797a6d4aef367a814ac398ad5e07a986
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-09-03 00:26:52 +0530

    Time: 3 ms (13.45%), Space: 19.3 MB (50.33%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -14,4 +14,26 @@ class Solution:
         valid_c = len(stack) if stack else 0
         
         return count +valid_c
+
+
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+
+        count = 0
+
+        ans = 0
+
+        for ch in s:
+
+            if ch =="(":
+                count +=1
+            else:
+                if count>0:
+                    count-=1
+                else:
+                    ans+=1
+
+        return count+ans
+
+    
         
