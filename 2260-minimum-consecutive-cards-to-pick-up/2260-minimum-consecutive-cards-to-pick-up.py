f2a601e2f300071a5ac65b73f71bb92cb678a3d5
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-09-11 19:58:46 +0530

    Time: 64 ms (91.43%), Space: 37.7 MB (38.15%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.py
@@ -15,3 +15,19 @@ class Solution:
 
         return ans
         
+
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        dp = {}
+        ans = float("+inf")
+
+        for index, val in enumerate(cards):
+            if val in dp:
+                ans = min(ans,-dp[val]+index +1)
+
+            dp[val] = index
+
+        if ans == float("+inf"):
+            return -1
+
+        return ans
