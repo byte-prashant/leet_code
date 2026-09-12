21bdcca1f6ba4979c7029131deeea17a0092e105
Prashant verma <44024191+byte-prashant@users.noreply.github.com>
2026-09-13 01:21:21 +0530

    Time: 59 ms (40.03%), Space: 36.5 MB (24.19%) - LeetHub

--- 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.py
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.py
@@ -13,3 +13,23 @@ class Solution:
             else:
                 queries[pos] = -1
         return queries
+
+
+class Solution:
+    def occurrencesOfElement(self, nums: List[int], queries: List[int], x: int) -> List[int]:
+
+        dp = {}
+        ocu = 0
+        for index, num in enumerate(nums):
+            if num == x:
+                ocu+=1
+                dp[ocu] =index
+        
+        ans = []
+        for q in queries:
+            if q in dp:
+                ans.append(dp[q])
+            else:
+                ans.append(-1)
+
+        return ans
